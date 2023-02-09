c6b5825036d24c7038612a935fd4dad9d293960b
KarimeLinhares <karime_muniz@hotmail.com>
2023-02-09 12:35:59 -0300

    basic calculator

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import Calculation from './Components/Calculation';
 
 function App() {
-	return <div className='App'>My calculator</div>;
+	return (
+		<div className='App'>
+			<h1>My Calculator</h1>
+			<Calculation />
+		</div>
+	);
 }
 
 export default App;
